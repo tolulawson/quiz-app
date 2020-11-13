c45812740fbb26a87545f449b99978afd78568b5
Tolu Lawson <lawsontolu1@gmail.com>
2020-11-13 11:27:27 +0100

    -- Eidt icons and splashscreen images

--- pages/_document.js
+++ pages/_document.js
@@ -10,26 +10,34 @@ class MyDocument extends Document {
     return (
       <Html manifest="/cache.manifest">
         <Head />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
-        <title>Live Capture by Future Wox</title>
+        <title>Sanofi Quiz</title>
 
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-        <meta name="description" content="Collect, store and retrieve data at live events and on the field, offline-first." />
+        <meta name="description" content="An app for administering quizes by Sanofi medical representatives to their stakeholders." />
         <meta name="keywords" content="Keywords" />
-        <meta name="apple-mobile-web-app-title" content="Live Capture" />
+        <meta name="apple-mobile-web-app-title" content="Sanofi Quiz" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="screen-orientation" content="portrait" />
-        <meta name="theme-color" content="#EE493E" />
+        <meta name="theme-color" content="#2E1D5F" />
 
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
         <link rel="apple-touch-startup-image" href="/icons/launch.png" />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+          href="/icons/launch-landscape.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+          href="/icons/launch.png"
+        />
 
 
         <link href="/manifest.json" rel="manifest" />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Link from 'next/link';
-import Dexie from 'dexie';
 import React from 'react';
-import { useRouter } from 'next/router';
 
 export default function Home() {
   return (
