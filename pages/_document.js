a943c7e442e56cc957ce71d7c22973a44ab094ca
Tolu Lawson <lawsontolu1@gmail.com>
2020-11-22 16:09:06 +0100

    -- Add pwa splashcreen

--- pages/_document.js
+++ pages/_document.js
@@ -31,16 +31,10 @@ class MyDocument extends Document {
         <link rel="apple-touch-icon" sizes="1024x1024" href="/icons/1024.png" />
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/167.png" />
 
-        <link
-          rel="apple-touch-startup-image"
-          media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-          href="/icons/launch-landscape.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-          href="/icons/launch.png"
-        />
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="/icons/launch-landscape.png">
+
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="/icons/launch.png">
+
 
 
         <link href="/manifest.json" rel="manifest" />
