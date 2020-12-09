76b29b035593a423d27f7edbdd9333b59f1889ce
Tolu Lawson <lawsontolu1@gmail.com>
2020-12-09 20:15:12 +0100

    -- Font import edit

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
       <Html manifest='/cache.manifest'>
         <Head />
         <title>Sanofi Quiz</title>
-        <link href='http://fonts.cdnfonts.com/css/sf-ui-display' rel='stylesheet' />
+        <link href='https://fonts.cdnfonts.com/css/sf-ui-display' rel='stylesheet' />
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
