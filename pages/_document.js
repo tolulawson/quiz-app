17cf2d8b7bc99d7071864de435af588abaa7d95a
Tolu Lawson <lawsontolu1@gmail.com>
2021-01-01 15:11:58 +0100

    -- Manifest changes

--- pages/_document.js
+++ pages/_document.js
@@ -17,28 +17,16 @@ class MyDocument extends Document {
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-        <meta name='description' content='An app for administering quizes by Sanofi medical representatives to their stakeholders.' />
+        <meta name='description' content='An app for administering quizzes by Sanofi medical representatives to their stakeholders.' />
         <meta name='keywords' content='Keywords' />
         <meta name='apple-mobile-web-app-title' content='Sanofi Quiz' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='screen-orientation' content='portrait' />
         <meta name='theme-color' content='#2E1D5F' />
-
-        <link rel='apple-touch-icon' href='/icons/192.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/180.png' />
-        <link rel='apple-touch-icon' sizes='512x512' href='/icons/512.png' />
-        <link rel='apple-touch-icon' sizes='512x512' href='/icons/512.png' />
-        <link rel='apple-touch-icon' sizes='512x512' href='/icons/512.png' />
-        <link rel='apple-touch-icon' sizes='1024x1024' href='/icons/1024.png' />
-        <link rel='apple-touch-icon' sizes='167x167' href='/icons/167.png' />
-
-        <link rel='apple-touch-startup-image' media='screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)' href='/icons/launch-landscape.png' />
-
-        <link rel='apple-touch-startup-image' media='screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)' href='/icons/launch.png' />
-
         <link href='/manifest.json' rel='manifest' />
+        <script async src='https://unpkg.com/pwacompat' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' href='icons/144.png' sizes='144x144' />
         <body>
           <Main />
           <NextScript />
